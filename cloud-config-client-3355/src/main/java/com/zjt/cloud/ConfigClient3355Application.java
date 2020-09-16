fa84746d2e17bb8be57ef5fd5c5905c7c3a994ba
jtz <jtz>
2020-09-16 16:20:19 +0800

    spring cloud config

---
+++ cloud-config-client-3355/src/main/java/com/zjt/cloud/ConfigClient3355Application.java
@@ -0,0 +1,23 @@
+package com.zjt.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import java.util.TimeZone;
+
+/**
+ * @author zjt
+ * @date 2020-09-16
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClient3355Application {
+
+    public static void main(String[] args) {
+        // 时区设置
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+        SpringApplication.run(ConfigClient3355Application.class, args);
+    }
+
+}
