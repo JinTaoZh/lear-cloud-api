38ea635c9210373a3a3743653ddf851705ac4ff7
jtz <jtz>
2020-09-25 10:48:59 +0800

    nacos 服务注册中心 服生产者

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/zjt/cloud/NacosProvider9001Application.java
@@ -0,0 +1,23 @@
+package com.zjt.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import java.util.TimeZone;
+
+/**
+ * @author zjt
+ * @date 2020-09-21
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosProvider9001Application {
+
+    public static void main(String[] args) {
+        // 时区设置
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+        SpringApplication.run(NacosProvider9001Application.class, args);
+    }
+
+}
